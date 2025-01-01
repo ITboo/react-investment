946b747b010526fd620399ba79f1a378aff99422
ITboo <annaacdc@tut.by>
2025-01-01 21:09:06 +0300

    chore: update actions in reducer

--- src/redux/slices/calculatorSlice.ts
+++ src/redux/slices/calculatorSlice.ts
@@ -1,6 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { v4 as uuidv4 } from "uuid";
+import { transliterate as tr } from "transliteration";
 
-import { CalculatorT } from "../../types";
+import {
+  CATEGORIES,
+  CalculatorT,
+  EntryT,
+  keyIsArrayCategory,
+} from "../../types";
 
 const initialState: CalculatorT = {
   income: [],
@@ -19,8 +26,33 @@ const initialState: CalculatorT = {
 export const calculatorSlice = createSlice({
   name: "calculator",
   initialState,
-  reducers: {},
+  reducers: {
+    addEntry: (
+      state,
+      action: PayloadAction<{ category: string; type: string }>
+    ) => {
+      const { category, type } = action.payload;
+      if (!keyIsArrayCategory(category)) return;
+      state[category].push({
+        id: uuidv4(),
+        value: 0,
+        type,
+        name: tr(type),
+      });
+    },
+    deleteEntry: (state, action: PayloadAction<EntryT["id"]>) => {
+      for (const category of CATEGORIES) {
+        const targetIndex = state[category].findIndex(
+          (entry) => entry.id === action.payload
+        );
+        if (targetIndex !== -1) {
+          state[category].splice(targetIndex, 1);
+          return;
+        }
+      }
+    },
+  },
 });
-export const {} = calculatorSlice.actions;
+export const { addEntry, deleteEntry } = calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
